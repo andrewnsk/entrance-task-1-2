dfbdd0c1e27f27339f8990e47bb7fe3a920035ff
Andrew Dorokhin <andrew@dorokhin.moscow>
2018-08-27 14:06:43 +0700

    add export, fix ymaps clustering

--- src/map.js
+++ src/map.js
@@ -2,7 +2,7 @@ import { loadList, loadDetails } from './api';
 import { getDetailsContentLayout } from './details';
 import { createFilterControl } from './filter';
 
-export function initMap(ymaps, containerId) {
+function initMap(ymaps, containerId) {
   const myMap = new ymaps.Map(containerId, {
     center: [55.76, 37.64],
     controls: [],
@@ -14,15 +14,17 @@ export function initMap(ymaps, containerId) {
     gridSize: 64,
     clusterIconLayout: 'default#pieChart',
     clusterDisableClickZoom: false,
-    geoObjectOpenBalloonOnClick: false,
+    clusterIconPieChartStrokeWidth: 2,
+    geoObjectOpenBalloonOnClick: true,
     geoObjectHideIconOnBalloonOpen: false,
     geoObjectBalloonContentLayout: getDetailsContentLayout(ymaps)
   });
 
-  objectManager.clusters.options.set('preset', 'islands#greenClusterIcons');
+  objectManager.clusters.options.set('preset', 'default#pieChart');
 
   loadList().then(data => {
     objectManager.add(data);
+    myMap.geoObjects.add(objectManager);
   });
 
   // details
@@ -30,12 +32,11 @@ export function initMap(ymaps, containerId) {
     const objectId = event.get('objectId');
     const obj = objectManager.objects.getById(objectId);
 
-    objectManager.objects.balloon.open(objectId);
-
     if (!obj.properties.details) {
       loadDetails(objectId).then(data => {
         obj.properties.details = data;
         objectManager.objects.balloon.setData(obj);
+        objectManager.objects.balloon.open(objectId);
       });
     }
   });
@@ -51,3 +52,5 @@ export function initMap(ymaps, containerId) {
     );
   });
 }
+
+export default initMap;
